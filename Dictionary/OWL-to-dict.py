b526da713aaa1fc1e8a7071398cddd933156d550
Marcus Medom Ryding <mrok4a@gmail.com>
2018-10-21 13:25:16 +0200

    Turn symptoms ontology into a dict of symptoms
    The `Symptom` class should hopefully be complete,
    otherwise it shouldn't be too hard to extend.
    
    The script's structure is too symptom-specific
    for it to make sense to include any arbitrary ontology,
    so I removed the DISCUSS item.

--- Dictionary/OWL-to-dict.py
+++ Dictionary/OWL-to-dict.py
@@ -1,4 +1,27 @@
 import owlready2 as o
+import sys
 
-onto_symps = o.get_ontology("file://symp.owl") # DISCUSS make this a commandline argument?
+onto_symps = o.get_ontology("file://symp.owl")
 onto_symps.load()
+
+class Symptom():
+    def __init__(self, internal_id, onto_symp):
+        self.internal_id = internal_id
+        self.symp_id = onto_symp.id[0]
+        self.names = onto_symp.label + onto_symp.hasExactSynonym + symp.hasRelatedSynonym
+        self.parents = onto_symp.ancestors() - {onto_symp}
+        self.other_ids = onto_symp.hasDbXref
+
+symps = {}
+
+# TODO tackle alternative IDs (hasAlternativeId)
+
+# TODO find out the proper pattern for ids
+curr_id = 0
+for symp in onto_symps.classes():
+    if not symp.deprecated: # DISCUSS should we include deprecated symptoms?
+        if not symp.id:
+            print("Warning: symptom",symp.name,"is missing a SYMP id, creating one automatically", file=sys.stderr)
+            symp.id = ['SYMP:' + symp.name[5:]]
+        symps[curr_id] = Symptom(curr_id, symp)
+        curr_id = curr_id + 1
